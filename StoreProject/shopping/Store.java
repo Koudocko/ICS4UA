2a705b24380cc89f110530ba8ebef052c5735e2b
Koudocko <tyler@pcquest.ca>
2022-09-28 19:43:21 -0400

    Added displayInventory and addItem methods to Store class.

--- StoreProject/shopping/Store.java
+++ StoreProject/shopping/Store.java
@@ -1,16 +1,50 @@
 package shopping;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 class Item{
 	public String name;
-	public int stock, sale;
+	public int stock, salePercent;
 	public double price;
+
+	public Item(String name, int stock, double price, int salePercent){
+		this.name = name;
+		this.stock = stock;
+		this.price = price;
+		this.salePercent = salePercent;
+	}
 }
 
 public class Store{
 	private ArrayList<Item>	inventory;
 	public String name, address, phoneNumber, postalCode;
-	public int inventorySize;
-	public double totalRevenue;
+
+	public Store(String name, String address, String phoneNumber, String postalCode){
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.postalCode = postalCode;
+	}
+
+	public void displayInventory(){
+		System.out.println("--------------------------------------------------");
+
+		for (int i = 0; i < inventory.size(); ++i){
+			System.out.println("|");
+			System.out.print("| Name: " + inventory.get(i).name);
+			System.out.print("| Stock: " + inventory.get(i).stock);
+			System.out.print("| Price: $" + inventory.get(i).price);
+			System.out.println("|");
+		}
+
+		System.out.println("--------------------------------------------------");
+	}
+
+	public void addItem(String name, double price){
+		Random rand = new Random();
+		int salePercent = rand.nextBoolean() ? rand.nextInt(100) + 1 : 0;
+
+		inventory.add(new Item(name, rand.nextInt(50) + 1, price, salePercent));
+	}
 }
